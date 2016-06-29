f25c32935ade2ccd9de368cb055feb5feeef45e1
Gabriel Santos <Spartan0799@users.noreply.github.com>
2016-06-28 21:24:41 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SinalDeTrânsito_Gabriel_2003
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Verde");
+            Console.ReadLine();
+
+            Console.WriteLine("Giallo");
+            Console.ReadLine();
+
+            Console.WriteLine("Rosso");
+            Console.ReadLine();
+            Console.Clear();
+        }
+    }
+}
